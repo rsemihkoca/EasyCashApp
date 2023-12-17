b270e392e5f177c77649f2d11e52c88761d384ce
Riza Semih Koca <rsemihkoca@outlook.com>
2023-12-17 16:22:44 +0300

    AppUserRegisterRequest

--- EasyCashIdentityProject/DtoLayer/Dtos/AppUserDtos/AppUserRegisterRequest.cs
+++ EasyCashIdentityProject/DtoLayer/Dtos/AppUserDtos/AppUserRegisterRequest.cs
@@ -2,6 +2,17 @@ namespace DtoLayer.Dtos.AppUserDtos
 {
     public record AppUserRegisterRequest
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public int Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
 
     }
 }
