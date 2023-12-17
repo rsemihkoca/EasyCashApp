7d8b549741fe815d591627ede9fab0a2a3600753
Riza Semih Koca <rsemihkoca@outlook.com>
2023-12-17 22:00:23 +0300

    Username error fix DTO

--- EasyCashIdentityProject/DtoLayer/Dtos/AppUserDtos/AppUserRegisterRequest.cs
+++ EasyCashIdentityProject/DtoLayer/Dtos/AppUserDtos/AppUserRegisterRequest.cs
@@ -8,7 +8,7 @@ namespace DtoLayer.Dtos.AppUserDtos
 
         public string Email { get; set; }
 
-        public int Username { get; set; }
+        public string Username { get; set; }
 
         public string Password { get; set; }
 
