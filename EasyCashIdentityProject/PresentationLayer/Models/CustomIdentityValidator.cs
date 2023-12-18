1f9987b616de21732be4bf262c96410db0d47881
Riza Semih Koca <rsemihkoca@outlook.com>
2023-12-18 19:34:27 +0300

    translate error

---
+++ EasyCashIdentityProject/PresentationLayer/Models/CustomIdentityValidator.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PresentationLayer.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = nameof(PasswordTooShort),
+                Description = $"Şifre en az {length} karakter olmalıdır."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = nameof(PasswordRequiresUpper),
+                Description = $"Şifre en az bir büyük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description = $"Şifre en az bir küçük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = nameof(PasswordRequiresDigit),
+                Description = $"Şifre en az bir rakam içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = nameof(PasswordRequiresNonAlphanumeric),
+                Description = $"Şifre en az bir alfanümerik olmayan karakter içermelidir."
+            };
+        }
+        
+    }
+}
