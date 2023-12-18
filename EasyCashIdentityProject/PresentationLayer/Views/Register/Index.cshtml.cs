ca273d2c1b1fbec677bed2567e0023fc7cb8218c
Riza Semih Koca <rsemihkoca@outlook.com>
2023-12-18 16:59:43 +0300

    register page

--- EasyCashIdentityProject/PresentationLayer/Views/Register/Index.cshtml.cs
+++ EasyCashIdentityProject/PresentationLayer/Views/Register/Index.cshtml.cs
@@ -1,24 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace PresentationLayer.Views.Register
+namespace PresentationLayer.Controllers;
+
+public class Index : PageModel
 {
-    public class Index : PageModel
+    public void OnGet()
     {
-        private readonly ILogger<Index> _logger;
-
-        public Index(ILogger<Index> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+        
     }
 }
