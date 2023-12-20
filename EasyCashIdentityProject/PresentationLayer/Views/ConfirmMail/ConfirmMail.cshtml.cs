a23e1f6ed11b8cddd94229e79ab3e88db0e83402
Riza Semih Koca <rsemihkoca@outlook.com>
2023-12-20 15:10:26 +0300

    confirm mail and style

---
+++ EasyCashIdentityProject/PresentationLayer/Views/ConfirmMail/ConfirmMail.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PresentationLayer.Views.ConfirmMail;
+
+public class ConfirmMail : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
