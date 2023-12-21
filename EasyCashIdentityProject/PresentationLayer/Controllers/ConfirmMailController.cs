a4281b6024af8d06195c588ce489ea3a84466ec3
Riza Semih Koca <rsemihkoca@outlook.com>
2023-12-21 16:09:34 +0300

    Confirm Mail

--- EasyCashIdentityProject/PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject/PresentationLayer/Controllers/ConfirmMailController.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace PresentationLayer.Controllers
 {
@@ -22,7 +16,7 @@ namespace PresentationLayer.Controllers
         public IActionResult Index()
         {
             var value = TempData["Mail"];
-            return View();
+            return View(value);
         }
 
         [HttpPost]
