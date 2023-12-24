4f013f4396babbd5d6610c8c17ea657b3b82bc72
Riza Semih Koca <rsemihkoca@outlook.com>
2023-12-24 17:30:47 +0300

    sidebar partial

---
+++ EasyCashIdentityProject/PresentationLayer/ViewComponents/Customer/_SidebarPartial.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.ViewComponents.Customer;
+
+public class _SidebarPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
